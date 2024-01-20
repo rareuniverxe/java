f3bb17621e4cbb64ed101d37f69fd3d6b501370a
rareuniverxe <ranjithreddyaleti1@gmail.com>
2024-01-20 11:45:14 -0600

    My first git hub push

---
+++ Fibonacci.java
@@ -0,0 +1,31 @@
+public class Fibonacci {
+	public static void main(String[] args) {
+	int a=1;
+	int b=1;
+	System.out.println(a);
+	System.out.println(b);
+	int c; // 1 1 2 3 5 8 ...
+	for(int i=0; i<10; i++) {
+		c = a+b;
+		System.out.println(c);
+		a = b;
+		b = c;
+	}
+	}
+}
+
+/* 
+8 = 5 + 3;
+
+5 = 3 + 2;
+
+3 = 2 + 1;
+
+2 = 1 + 1;
+
+1 = 1 + 0;
+
+0 = 0 + 0;
+
+*/
+
---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+	public static void main(String[] args) {
+		for(int i =1; i<=1000; i++) {
+		if(i%5==0) {
+		System.out.println("Fizz");
+		}
+		else if(i%7==0) {
+		System.out.println("Buzz");
+		}
+		else if(i%5==0 && i%7==0) {
+		System.out.println("Fizz Buzz");
+		}
+		else {
+		System.out.println(i);
+		}
+		}
+	}
+}
